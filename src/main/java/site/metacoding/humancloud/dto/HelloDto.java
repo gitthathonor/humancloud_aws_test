489b2b06189c6717db94da52fb700952fa4861ff
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-10 17:26:05 +0900

    merge 시에 배포 되도록 deploy.yml 설정 변경 테스트

---
+++ src/main/java/site/metacoding/humancloud/dto/HelloDto.java
@@ -0,0 +1,13 @@
+package site.metacoding.humancloud.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class HelloDto {
+    private Integer id;
+    private String name;
+}
